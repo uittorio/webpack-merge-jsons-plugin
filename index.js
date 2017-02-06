f5bae71f54f2679242822345335b157060997f24
vittorioguerriero <vittorio.gue@gmail.com>
2017-02-06 19:19:20 +0000

    #1 add catch to json parse so it will no crash

--- index.js
+++ index.js
@@ -64,9 +64,16 @@ WebpackMergeJsonsPlugin.prototype.addJsonToWebPackAssets = function(compilation,
 };
 
 WebpackMergeJsonsPlugin.prototype.addJsonToMergedJson = function(fileName, json) {
-  var fileContent = JSON.parse(fs.readFileSync(fileName, 'utf8'));
+  var file = fs.readFileSync(fileName, 'utf8');
 
-  Object.assign(json, fileContent);
+  try {
+    var fileContent = JSON.parse(file);
+
+    Object.assign(json, fileContent);
+
+  } catch(e) {
+
+  }
 };
 
 WebpackMergeJsonsPlugin.prototype.isFileChanged = function(compilation, filePath) {
